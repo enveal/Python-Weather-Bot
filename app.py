09d4a85f0ca47418825a89d95fe43e614ecb7136
enveal <envealsood@gmail.com>
2021-03-24 12:46:31 +0530

    Port changes v1

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def messages():
 
 if __name__ == '__main__':
     #app.run(port= 3978)
-     app.run(port=process.env.PORT or 5000) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #  app.run(port=process.env.PORT or 5000) 
